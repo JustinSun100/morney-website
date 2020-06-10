5f0166904d0a59b97e727f1f50d6a81284431dac
justinsun <sun_heqing@163.com>
2020-06-10 11:05:44 +0800

    deploy

---
+++ precache-manifest.e1373aca3d7143dfd96ef6fbc76814aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8313080d7c079473b704",
+    "url": "/morney-website/css/app.691d708f.css"
+  },
+  {
+    "revision": "e3c104e66c603d535a87e2e3f5be4888",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "8313080d7c079473b704",
+    "url": "/morney-website/js/app.1d5495d1.js"
+  },
+  {
+    "revision": "f3f5fa590a4ce9c6760c",
+    "url": "/morney-website/js/chunk-vendors.39221ced.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "4b436d44ea2500ebf6ec1d73cf417a2c",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
